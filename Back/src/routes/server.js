c97c5b7c79041de31f0e3e748b356570cb7d74aa
Santiago Pagge <santy.pagge@gmail.com>
2023-01-25 22:45:39 -0300

    server creado

---
+++ Back/src/routes/server.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const characters = require("../utils/data")
+
+http.createServer((req, res) => {
+
+    if(req.url.includes("rickandmorty/character")){
+        let id = req.url.split("/").at(-1);
+        // let characterFilter = characters.filter(char => char.id === Number(id))
+        let characterFilter = characters.find(char => char.id === Number(id)) 
+        // Filter y Find tienen la misma sintaxis, la diferencia es que Filter devuelve un array de objetos, y Find devuelve un Objeto
+
+        res.writeHead(200, {"Content-type":"application/json"}).end(JSON.stringify(characterFilter)) 
+        // la info viaja en formato JSON asi que hay que parsearla, con JSON.stringify()
+
+        
+    }
+
+}).listen(3001, "localhost")
